f8efc7fab258a4278a60eabc584aada5fe0e01ce
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:02:17 +0300

    Baikalify 1
    Change-Id: I7b1d1225a914eef9d1687dedf56fd50f23dd5b31

---
+++ src/ru/baikalos/settings/fragments/About.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2016-2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ru.baikalos.settings.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.android.settings.R;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import androidx.annotation.Keep;
+@Keep
+@SearchIndexable
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    private String KEY_CRDROID_SOURCE = "baikalos_source";
+    private String KEY_CRDROID_TELEGRAM = "baikalos_telegram";
+
+    private Preference mSourceUrl;
+    private Preference mTelegramUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.baikalos_settings_about);
+
+        mSourceUrl = findPreference(KEY_CRDROID_SOURCE);
+        mTelegramUrl = findPreference(KEY_CRDROID_TELEGRAM);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mSourceUrl) {
+            launchUrl("https://github.com/baikalos");
+        } else if (preference == mTelegramUrl) {
+            launchUrl("https://t.me/baikalos");
+        }
+
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(intent);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.baikalos_settings_about);
+}
